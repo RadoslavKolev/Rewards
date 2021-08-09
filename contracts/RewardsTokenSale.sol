ac586472090bb5c087e95b9cfff4f0779081f07d
Radoslav <radoslav.kolev@abv.bg>
2021-08-10 00:11:02 +0300

    5. Buy Tokens

--- contracts/RewardsTokenSale.sol
+++ contracts/RewardsTokenSale.sol
@@ -9,6 +9,11 @@ contract RewardsTokenSale {
     uint256 public tokenPrice;
     uint256 public tokensSold;
 
+    event Sell(
+        address _buyer,
+         uint256 _amount
+    );
+
     constructor(Rewards _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
@@ -20,8 +25,11 @@ contract RewardsTokenSale {
     }
 
     function buyTokens(uint256 _numberOfTokens) public payable {
+        // Require that value is equal to tokens
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(msg.sender) >= _numberOfTokens);
+        // Require that the contract has enough tokens
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        // Require a successful transaction
         require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
         tokensSold += _numberOfTokens;
@@ -29,10 +37,5 @@ contract RewardsTokenSale {
         emit Sell(msg.sender, _numberOfTokens);
     }
 
-    function endSale() public {
-        require(msg.sender == admin);
-        require(tokenContract.transfer(admin, tokenContract.balanceOf(msg.sender)));
-        
-        admin.transfer(address(this).balance);
-    }
+    
 }
--- test/RewardsTokenSale.js
+++ test/RewardsTokenSale.js
@@ -1,21 +1,67 @@
+var Rewards = artifacts.require('./Rewards.sol');
 var RewardsTokenSale = artifacts.require('./RewardsTokenSale.sol');
 
 contract('RewardsTokenSale', function(accounts) {
-    var tokenSaleInstance;
-    var tokenPrice = 1000000000000000;  // in wei
+  var tokenInstance;
+  var tokenSaleInstance;
+  var admin = accounts[0];
+  var buyer = accounts[1];
+  var tokenPrice = 1000000000000000; // in wei
+  var tokensAvailable = 250000000;
+  var numberOfTokens;
 
-    it('Initializes contract with the correct values', function() {
-        return RewardsTokenSale.deployed().then(function(instance) {
-            tokenSaleInstance = instance;
-            return tokenSaleInstance.address
-        }).then(function(address) {
-            assert.notEqual(address, 0x0, 'Has contract address');
-            return tokenSaleInstance.tokenContract();
-        }).then(function(address) {
-            assert.notEqual(address, 0x0, 'Has token contract address');
-            return tokenSaleInstance.tokenPrice();
-        }).then(function(price) {
-            assert.equal(price, tokenPrice, 'Token price is correct');
-        });
+  it('Initializes the contract with the correct values', function() {
+    return RewardsTokenSale.deployed().then(function(instance) {
+      tokenInstance = instance;
+        tokenSaleInstance = instance;
+      return tokenSaleInstance.address
+    }).then(function(address) {
+      assert.notEqual(address, 0x0, 'Has contract address');
+      return tokenSaleInstance.tokenContract();
+    }).then(function(address) {
+      assert.notEqual(address, 0x0, 'Has token contract address');
+      return tokenSaleInstance.tokenPrice();
+    }).then(function(price) {
+      assert.equal(price, tokenPrice, 'Token price is correct');
     });
+  });
+
+  it('Facilitates token buying', function() {
+    return Rewards.deployed().then(function(instance) {
+      // Grab token instance first
+      tokenInstance = instance;
+      return RewardsTokenSale.deployed();
+    }).then(function(instance) {
+      // Then grab token sale instance
+      tokenSaleInstance = instance;
+      // Provision 75% of all tokens to the token sale
+      return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
+    }).then(function(receipt) {
+      numberOfTokens = 10;
+      return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: numberOfTokens * tokenPrice })
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'Triggers one event');
+      assert.equal(receipt.logs[0].event, 'Sell', 'Should be the "Sell" event');
+      assert.equal(receipt.logs[0].args._buyer, buyer, 'Logs the account that purchased the tokens');
+      assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'Logs the number of tokens purchased');
+      return tokenSaleInstance.tokensSold();
+    }).then(function(amount) {
+      assert.equal(amount.toNumber(), numberOfTokens, 'Increments the number of tokens sold');
+      return tokenInstance.balanceOf(buyer);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), numberOfTokens);
+      return tokenInstance.balanceOf(tokenSaleInstance.address);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens);
+      // Try to buy tokens different from the ether value
+      return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+      return tokenSaleInstance.buyTokens(800000, { from: buyer, value: numberOfTokens * tokenPrice })
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'Cannot purchase more tokens than available');
+    });
+  });
+
+  
 });
